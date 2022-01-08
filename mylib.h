f7ed9b72f759b384ecb7139dcb11c9c4e1b9ba95
TinNguyen01600 <ductin16111@gmail.com>
2022-01-09 01:51:06 +0200

    add comment and create function loop()

--- mylib.h
+++ mylib.h
@@ -10,4 +10,5 @@ struct Date {
     int year;
 };
 
-void change_to_wday(int n);
+void change_to_wday(int n);     // This function change the weekday number calculated (0,1,2,...) into weekday name (Sunday, Monday, Tuesday, ...)
+void loop();                     // This function is the backbone of the program, which repeats itself until user exits
