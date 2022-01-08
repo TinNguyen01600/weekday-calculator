fabe8fa818c05e9def2c5615633664bf95bed7d6
TinNguyen01600 <ductin16111@gmail.com>
2022-01-09 01:42:52 +0200

    add macro definition enum week and function change_to_wday()

--- mylib.h
+++ mylib.h
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <windows.h>
 
+enum week{Sun = 1, Mon, Tue, Wed, Thu, Fri, Sat}; //macro definition
+
 struct Date {
     int wday;
     int day;
     int month;
     int year;
 };
+
+void change_to_wday(int n);
