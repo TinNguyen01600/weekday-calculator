6d034cde2e184aeff7ede97c806cb9cf55aee9c6
TinNguyen <ductin16111@gmail.com>
2022-04-01 00:38:40 +0300

    fix function leap_year() algorithm

--- non_task.c
+++ non_task.c
@@ -18,8 +18,8 @@ bool check_month_30(int month){
 }
 
 bool leap_year(int year){
-  if(year % 100 == 0 && year % 400 == 0){return true;}
-  else if(year % 4 == 0){return true;}
+  if(year % 400 == 0){return true;}
+  else if(year % 4 == 0 && year % 100 != 0){return true;}
   else return false;
 }
 
